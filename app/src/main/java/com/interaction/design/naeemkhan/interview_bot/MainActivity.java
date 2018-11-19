6602a50b95117b6dd851fc97f5ee7f13a495ab45
Naeem Khan <naeemukhan14@gmail.com>
2018-11-20 02:11:02 +0400

    Implemented HTML parser to get data from website and use Text-To-Speech with it

--- app/src/main/java/com/interaction/design/naeemkhan/interview_bot/MainActivity.java
+++ app/src/main/java/com/interaction/design/naeemkhan/interview_bot/MainActivity.java
@@ -4,16 +4,28 @@ import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     TextToSpeech TTS;
-    EditText textbox;
-    Button gobutton;
+    private TextView textview;
+    private Button gobutton;
+    private Spinner spinnerList;
+    private ArrayAdapter<String> adapter;
+    private final ArrayList<String> results = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +33,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Get the components of the app and assign them here for use
-        textbox = findViewById(R.id.text_box);
+        textview = findViewById(R.id.text_view);
         gobutton = findViewById(R.id.go_button);
+        // First value for TTS to play
+        results.add("Select a question from the list below");
 
         // Initialise Text-To-Speech
         TTS = new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
@@ -39,14 +53,64 @@ public class MainActivity extends AppCompatActivity {
         gobutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String textToSpeek = textbox.getText().toString();
-                /* Have the TTS play whatever is written in the text. Queue_flush is being used
-                *  to play the next text and cancel the ongoing speech if button is pressed
-                */
-                TTS.speak(textToSpeek, TextToSpeech.QUEUE_FLUSH, null);
+                getWebsiteData();
             }
         });
 
+    }
+
+    private void getWebsiteData() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // Connect to the link and get the results from specified tag
+                    Document dataSource = Jsoup.connect("http://shootboys.net/sss.html").get();
+                    Elements data = dataSource.select("body");
+                    // Split the results at ### so we know where a new line is
+                    String[] dataList = data.text().split("###");
+
+                    // Append each question into the ArrayList
+                    for(String value : dataList) {
+                        results.add(value);
+                    }
+
+                } catch (IOException e) {
+                    e.getMessage();
+                }
+
+                // Once the results' list is populated, push the result into spinner
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Initialize the spinner and enable it
+                        spinnerList = findViewById(R.id.spinner);
+                        spinnerList.setEnabled(true);
+                        spinnerList.setSelection(0, false);
+                        // ArrayAdapter will handle the values that goes into spinner
+                        adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, results);
+                        spinnerList.setAdapter(adapter);
+
+                        /*
+                        * Whenever an item from spinner is selected, this listener will record that
+                        * selection and display it in the spinner, as well as playing it
+                        */
+                        spinnerList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                            @Override
+                            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                textview.setText(results.get(position));
+                                TTS.speak(results.get(position), TextToSpeech.QUEUE_FLUSH, null);
+                            }
+
+                            @Override
+                            public void onNothingSelected(AdapterView<?> parent) {
+
+                            }
+                        });
+                    }
+                });
+            }
+        }).start();
     }
 
     // Force shutdown the TTS when app is closed
